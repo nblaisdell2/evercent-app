6ff926b643e25e89531c3f6fc694c38a5ca08386
Nicolas Blaisdell <nblaisdell2@gmail.com>
2023-10-15 21:45:51 -0400

    fixed loading screen?

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { log } from "./utils/log";
 
@@ -9,8 +9,14 @@ import Header from "./components/Header";
 function App() {
   log("RENDERING [App.tsx]");
 
+  // This is the code returned from Auth0 when logging in.
+  // This will force a reload, and flash the page beforehand, so checking
+  // for this URL param will allow me to continue showing the loading screen
+  // until the user is fully logged in
+  const stillLoading = new URLSearchParams(window.location.search).has("code");
+
   const { isLoading } = useAuth0();
-  if (isLoading) return <LoadingScreen />;
+  if (isLoading || stillLoading) return <LoadingScreen />;
 
   return (
     <div className={`flex flex-col h-screen bg-primary text-color-primary`}>
